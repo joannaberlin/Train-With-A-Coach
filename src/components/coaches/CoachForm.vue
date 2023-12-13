d7a25bf1c72f3f7adc214de600e7ed2dff7ba999
joannaberlin <joanna.kot.ok@gmail.com>
2023-12-13 14:23:05 +0100

    Add form validation in CoachForm

--- src/components/coaches/CoachForm.vue
+++ src/components/coaches/CoachForm.vue
@@ -1,32 +1,56 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div class="form-control" :class="{ invalid: !firstName.isValid }">
       <label for="firstname">Firstname</label>
-      <input type="text" id="firstname" v-model.trim="firstName.val" />
+      <input
+        type="text"
+        id="firstname"
+        v-model.trim="firstName.val"
+        @blur="clearValidity('firstName')"
+      />
+      <p v-if="!firstName.isValid">Firstname must not be empty.</p>
     </div>
-    <div class="form-control">
+    <div class="form-control" :class="{ invalid: !lastName.isValid }">
       <label for="lastname">Lastname</label>
-      <input type="text" id="lastname" v-model.trim="lastName.val" />
+      <input
+        type="text"
+        id="lastname"
+        v-model.trim="lastName.val"
+        @blur="clearValidity('lastName')"
+      />
+      <p v-if="!lastName.isValid">Lastname must not be empty.</p>
     </div>
-    <div class="form-control">
+    <div class="form-control" :class="{ invalid: !description.isValid }">
       <label for="description">Description</label>
       <textarea
         id="description"
         rows="5"
         v-model.trim="description.val"
+        @blur="clearValidity('description')"
       ></textarea>
+      <p v-if="!description.isValid">Description must not be empty.</p>
     </div>
-    <div class="form-controls">
+    <div class="form-controls" :class="{ invalid: !rate.isValid }">
       <label for="rate">Hourly Rate</label>
-      <input type="number" id="rate" v-model.number="rate.val" />
+      <input
+        type="number"
+        id="rate"
+        v-model.number="rate.val"
+        @blur="clearValidity('rate')"
+      />
+      <p v-if="!rate.isValid">Hourly Rate must be greater than 0.</p>
     </div>
-    <div class="form-control">
+    <div
+      class="form-control"
+      :class="{ invalid: !types.isValid }"
+      @blur="clearValidity('types')"
+    >
       <h3>Types of workouts offered:</h3>
       <div>
         <input
           type="checkbox"
           id="strength"
-          value="strngth"
+          value="strength"
           v-model="types.val"
         />
         <label for="strength">Strength workout</label>
@@ -44,8 +68,13 @@
         <input type="checkbox" id="HIIT" value="HIIT" v-model="types.val" />
         <label for="HIIT">HIIT workout</label>
       </div>
+      <p v-if="!types.isValid">At least one type must be selected.</p>
     </div>
-    <div class="form-control">
+    <div
+      class="form-control"
+      :class="{ invalid: !levels.isValid }"
+      @blur="clearValidity('levels')"
+    >
       <h3>Levels:</h3>
       <div>
         <input
@@ -65,8 +94,13 @@
         />
         <label for="advanced">Advanced</label>
       </div>
+      <p v-if="!levels.isValid">At least one level must be selected.</p>
     </div>
-    <div class="form-control">
+    <div
+      class="form-control"
+      :class="{ invalid: !genders.isValid }"
+      @blur="clearValidity('genders')"
+    >
       <h3>Who is it for?</h3>
       <div>
         <input
@@ -90,6 +124,7 @@
         />
         <label for="everyone">Everyone</label>
       </div>
+      <p v-if="!genders.isValid">At least one gender must be selected.</p>
     </div>
     <base-button>Register</base-button>
   </form>
@@ -132,7 +167,47 @@ export default {
     };
   },
   methods: {
+    clearValidity(input) {
+      this[input].isValid = true;
+    },
+    validateForm() {
+      this.formIsValid = true;
+      if (this.firstName.val === '') {
+        this.firstName.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.lastName.val === '') {
+        this.lastName.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.description.val === '') {
+        this.description.isValid = false;
+        this.formIsValid = false;
+      }
+      if (!this.rate.val || this.rate.val < 0) {
+        this.rate.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.types.val.length === 0) {
+        this.types.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.levels.val.length === 0) {
+        this.levels.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.genders.val.length === 0) {
+        this.genders.isValid = false;
+        this.formIsValid = false;
+      }
+    },
     submitForm() {
+      this.validateForm();
+
+      if (!this.formIsValid) {
+        return;
+      }
+
       const formData = {
         firstName: this.firstName.val,
         lastName: this.lastName.val,
@@ -195,5 +270,14 @@ input[type='checkbox']:focus {
 input[type='checkbox']:checked {
   accent-color: #8d0084;
   outline: #8d0084 solid 1px;
+}
+
+.invalid label {
+  color: red;
+}
+
+.invalid input,
+.invalid textarea {
+  border: 1px solid red;
 }
 </style>
